31c999381f896e8a33aa0324c1abb51fc8ee4355
Yahia Menter <y_menter@estin.dz>
2025-08-26 17:22:43 +0100

    *New Modifications:
    -Completing CSS style of the project and make it responsive
    -Using JS to manipulate the switch button between login and register pages
    -Modifiying HTML file so it will be suitable for the CSS and JS files

--- login_register.js
+++ login_register.js
@@ -1,3 +1,9 @@
-import { a } from "./main.js";
-
-console.log(a);
+let Switches = document.getElementsByClassName("switch");
+let loginContainer = document.getElementById("login");
+let registerContainer = document.getElementById("registration");
+Switches[0].addEventListener("click",SwitchButton);
+Switches[1].addEventListener("click",SwitchButton);
+function SwitchButton(){
+    loginContainer.classList.toggle("hidden");
+    registerContainer.classList.toggle("hidden");
+}
